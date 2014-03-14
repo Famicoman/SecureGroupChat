156da59273a7c9080af744b568846ffad4b2301a
Mike Dank <Famicoman@gmail.com>
2014-03-14 11:43:21 -0400

    Client/server now runs for multiple clients!
    Make sure you ssh to the same tux node for server and all clients!!!
    
    Need to work on:
    - Text formatting
    - Exiting doesn't work
    - Switch port to SSL
    - Implement some crypto

--- cliserv.py
+++ cliserv.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #Hybrid Client/Server
 import random, math
 import fractions
@@ -7,30 +8,34 @@ import string
 import time
 import threading
 
+
+#Make socket
+srvsock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 exit=0
+client_list={}
 
 class sending(threading.Thread):
-    def run(self):
-		print "type to send an excrypted message."
-        data = ""
-        while send.startswith("/q")==False:
-            data = sys.stdin.readline()
+	def run(self):
+		print "Type to send an excrypted message."
+        	data = ""
+        	while data.startswith("/q")==False:
+            		data = sys.stdin.readline()
 			if (sys.argv[1] == "s"):
 				for key in client_list:
-					clientlist[key].send(data)
+					client_list[key].send(data)
 			else:
 				clisock.send(data) #Send command
-        exit = 1
-		clisock.close()
-		self.join()
+		exit = 1
+		#clisock.close()
+		#self.join()
 		
 class receiving(threading.Thread):
-    def run(self):
-        while exit==0:
-            data = clisock.recv(1024)
-            print data #Print message from server
-		self.connection.close()
-		self.join()
+	def run(self):
+		while exit==0:
+            		data = clisock.recv(1024)
+			print data #Print message from server
+		clisock.close()
+		#self.join()
 
 class servreceiving(threading.Thread):
 
@@ -38,28 +43,30 @@ class servreceiving(threading.Thread):
 		self.address = address
 		self.connection = connection
 		threading.Thread.__init__(self)
-		
-    def run(self):
-        while exit==0:
-            data = self.connection.recv(1024)
-            print data #Print message from server
+	
+	def run(self):
+		while exit==0:
+			data = self.connection.recv(1024)
+			print data #Print message from server
 			for key in client_list:
-				clientlist[key].send(data)
+				client_list[key].send(data)
 		self.join()
 				
 class newconnection(threading.Thread):
-	global client_list = dict()
-	while 1:
-		connection, address = srvsock.accept()
-		client_list[address] = connection
-		servrec = servreceiving(address,connection)
-		servrec.start()
+	 def run(self):
+		print "Server listening for connections"
+		while 1:
+			connection, address = srvsock.accept()
+			print "[NEW CLIENT CONNECTED]"
+			client_list[address] = connection
+			servrec = servreceiving(address,connection)
+			servrec.start()
 
 		
 # ./securegroupchat s
 if (sys.argv[1] == "s"):
 	#Make socket
-	srvsock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+	#srvsock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 	srvsock.bind( ("127.0.0.1", 31337)) 
 	srvsock.listen( 5 )
 	
@@ -72,8 +79,8 @@ if (sys.argv[1] == "s"):
 if (sys.argv[1] == "c"):
 	#clisock, (remhost, remport) = srvsock.accept()
 	clisock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-	clisock.connect( (sys.argv[2], 31337) )
-	
+	clisock.connect( ("127.0.0.1", 31337) )
+	print "[CONNECTED TO SERVER]"	
 	r = receiving()
 	r.start()
 	s = sending()
